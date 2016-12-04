dc4275b0cafaf764c03b36b0ba1d975ded58d3d3
RodrigoNdeC <rodrigondec@gmail.com>
2016-12-04 17:49:54 -0300

    urls ok

--- bazarzero84/urls.py
+++ bazarzero84/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^\Z', include('bazar.urls')),
+    # url(r'^\Z', include('bazar.urls')),
     # url(r'^$', include('bazar.urls')),
-    # url(r'^bazar/', include('bazar.urls')),
+    url(r'^bazar/', include('bazar.urls')),
 ]
