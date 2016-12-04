8e68b408fcbde92a1b6b5984f2bbd40f7ca5aea3
RodrigoNdeC <rodrigondec@gmail.com>
2016-12-04 18:36:57 -0300

    env funcionando

--- bazarzero84/settings.py
+++ bazarzero84/settings.py
@@ -62,10 +62,21 @@ WSGI_APPLICATION = 'bazarzero84.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+PATH_CNF = os.path.join(BASE_DIR, 'mysql.cnf')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': PATH_CNF,
+        },
     }
 }
 
