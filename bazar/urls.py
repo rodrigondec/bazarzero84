e2148ddad12914d1056b9d7aaa50a9dc453dbae6
RodrigoNdeC <rodrigondec@gmail.com>
2016-12-05 17:49:56 -0300

    views atualizadsa

--- bazar/urls.py
+++ bazar/urls.py
@@ -3,5 +3,9 @@ from bazar import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^roupas/$', views.roupas, name='roupas'),
+    url(r'^calcados/$', views.calcados, name='calcados'),
+    url(r'^aderecos/$', views.aderecos, name='aderecos'),
+    url(r'^add_pessoa/$', views.add_pessoa, name='add_pessoa'),
 ]
         
--- bazar/views.py
+++ bazar/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from bazar.models import *
+from bazar.forms import PessoaForm
 
 def index(request):
 	tipo_roupa_list = Tipo_roupa.objects.all()
@@ -9,3 +10,51 @@ def index(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
 	return render(request, 'index.html', context_dict)
+
+def roupas(request):
+	tipo_roupa_list = Tipo_roupa.objects.all()
+	context_dict = {'tipo_roupas': tipo_roupa_list, 'boldmessage': "I am bold font from the context"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+	return render(request, 'roupas.html', context_dict)
+
+def calcados(request):
+	tipo_roupa_list = Tipo_roupa.objects.all()
+	context_dict = {'tipo_roupas': tipo_roupa_list, 'boldmessage': "I am bold font from the context"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+	return render(request, 'calcados.html', context_dict)
+
+def aderecos(request):
+	tipo_roupa_list = Tipo_roupa.objects.all()
+	context_dict = {'tipo_roupas': tipo_roupa_list, 'boldmessage': "I am bold font from the context"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+	return render(request, 'aderecos.html', context_dict)
+
+def add_pessoa(request):
+	# A HTTP POST?
+    if request.method == 'POST':
+        form = PessoaForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = PessoaForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'add_pessoa.html', {'form': form})
