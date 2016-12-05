63c4163cd813af76fd43edba2138bf1e0016bcdd
RodrigoNdeC <rodrigondec@gmail.com>
2016-12-05 18:59:30 -0300

    views ok

--- bazar/views.py
+++ bazar/views.py
@@ -6,33 +6,25 @@ from bazar.forms import PessoaForm
 def index(request):
 	tipo_roupa_list = Tipo_roupa.objects.all()
 	context_dict = {'tipo_roupas': tipo_roupa_list, 'boldmessage': "I am bold font from the context"}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+
 	return render(request, 'index.html', context_dict)
 
 def roupas(request):
-	tipo_roupa_list = Tipo_roupa.objects.all()
-	context_dict = {'tipo_roupas': tipo_roupa_list, 'boldmessage': "I am bold font from the context"}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-	return render(request, 'roupas.html', context_dict)
+    roupas = Roupa.objects.all()
+    context_dict = {'roupas': roupas}
+
+    return render(request, 'roupas.html', context_dict)
 
 def calcados(request):
 	tipo_roupa_list = Tipo_roupa.objects.all()
 	context_dict = {'tipo_roupas': tipo_roupa_list, 'boldmessage': "I am bold font from the context"}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+
 	return render(request, 'calcados.html', context_dict)
 
 def aderecos(request):
 	tipo_roupa_list = Tipo_roupa.objects.all()
 	context_dict = {'tipo_roupas': tipo_roupa_list, 'boldmessage': "I am bold font from the context"}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+
 	return render(request, 'aderecos.html', context_dict)
 
 def add_pessoa(request):
