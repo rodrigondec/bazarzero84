eac53b1d44792c963dac61f40c63c2ad7bb22dd5
RodrigoNdeC <rodrigondec@gmail.com>
2016-12-04 21:38:37 -0300

    adicionada interface admin

--- bazar/admin.py
+++ bazar/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from bazar.models import *
 
 # Register your models here.
+admin.site.register(Pessoa)
+admin.site.register(Produto)
+admin.site.register(Tipo_roupa)
+admin.site.register(Tipo_calcado)
+admin.site.register(Tipo_adereco)
+admin.site.register(Roupa)
+admin.site.register(Calcado)
+admin.site.register(Adereco)
