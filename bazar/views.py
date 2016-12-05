f482351f01d37ecee34fdff80794c2ae64b8c933
RodrigoNdeC <rodrigondec@gmail.com>
2016-12-05 16:27:10 -0300

    exemplo print lista de models

--- bazar/views.py
+++ bazar/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bazar.models import *
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-
+	tipo_roupa_list = Tipo_roupa.objects.all()
+	context_dict = {'tipo_roupas': tipo_roupa_list, 'boldmessage': "I am bold font from the context"}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-
-    return render(request, 'index.html', context_dict)
+	return render(request, 'index.html', context_dict)
