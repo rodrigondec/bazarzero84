f7b70af193ac7452a86fbac82755838014023986
Rodrigo Nunes de Castro <rodrigondec@gmail.com>
2016-12-08 21:58:36 -0200

    adicionado referência de luan

--- bazarzero84/urls.py
+++ bazarzero84/urls.py
@@ -7,3 +7,20 @@ urlpatterns = [
     # url(r'^$', include('bazar.urls')),
     url(r'^bazar/', include('bazar.urls')),
 ]
+
+# from django.conf.urls import patterns, include, url
+# from django.contrib import admin
+
+# admin.autodiscover()
+
+# urlpatterns = patterns(
+#     '',
+#     url(r'^admin/', include(admin.site.urls)),
+
+#     url(r'^accounts/', include('allauth.urls')),
+
+#     url(r'^', include('core.urls')),
+#     url(r'^', include('deck.urls')),
+#     url(r'^', include('jury.urls')),
+#     url(r'^api/', include('api.urls')),
+# )
