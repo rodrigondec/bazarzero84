51b2df48ebfe5291b987d2921ba5b6d8a7773b21
RodrigoNdeC <rodrigondec@gmail.com>
2016-12-18 00:25:43 -0300

    import static.static

--- bazarzero84/urls.py
+++ bazarzero84/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import include, url, static
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
